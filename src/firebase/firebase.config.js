d8771c6d447916eda90ddd3cabe3ab3954e90aca
Sharifpr <sharifpr532@gmail.com>
2021-10-09 21:39:10 +0600

    added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAFuLYhYt_9fNnYkdxseKIkJAgGnjwb7IQ",
+    authDomain: "ema-john-simple-2c521.firebaseapp.com",
+    projectId: "ema-john-simple-2c521",
+    storageBucket: "ema-john-simple-2c521.appspot.com",
+    messagingSenderId: "602999692837",
+    appId: "1:602999692837:web:b12c1702719b09fad3a9cc"
+};
+
+export default firebaseConfig;
